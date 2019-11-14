b0ee5e903683635ff56389388e929390dde5c444
IgorBarbashov <bis200705@gmail.com>
2019-11-14 06:47:30 +0300

    feat: add getLinkToImage function

---
+++ codejam-palette/src/js/api/unsplash.js
@@ -0,0 +1,22 @@
+const API_URL = 'https://api.unsplash.com';
+const GET_RND_PHOTO = '/photos/random';
+const API_ACCESS_KEY = 'b5fd9af85c9e3187e9d8a1fa3e90f844f3aca08b0b5593b82186881e55dd14e7';
+
+async function getLinkToImage(town = 'Nizhny Novgorod', size = 128) {
+  const resizeQuery = `fit=fill&fill=solid&fill-color=c4c4c4&w=${size}&h=${size}`;
+  try {
+    const response = await fetch(
+      `${API_URL}${GET_RND_PHOTO}?${resizeQuery}&query=town,${town}&client_id=${API_ACCESS_KEY}`,
+    );
+    if (!response.ok) {
+      throw new Error('Ошибка получения адреса картинки');
+    }
+    const data = await response.json();
+    const url = `${data.urls.custom.split('?')[0]}?${resizeQuery}`;
+    return url;
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+export default getLinkToImage;
